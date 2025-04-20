04da87667d6fcf4891898ad760f6665ea2c5db4b
nepticoo <mahdi.ab5310@gmail.com>
2025-04-21 00:12:25 +0330

    farmMap fixed

--- src/main/java/Modules/Map/FarmMap.java
+++ src/main/java/Modules/Map/FarmMap.java
@@ -9,8 +9,10 @@ public enum FarmMap {
 
     private final Size size;
     private final Position housePosition;
+    private final Size houseSize;
 //    4*4
     private final Position greenHousePosition;
+    private final Size greenHouseSize;
 //    5*6
     private final Position lakePosition;
     private final Size lakeSize;
@@ -18,7 +20,113 @@ public enum FarmMap {
     private final Size quarterSize;
 
     FarmMap(int id) {
+        size = new Size(100, 100);
+
+        switch (id) {
+            case 0: {
+                housePosition = new Position(12, 10);
+                houseSize = new Size(4, 4);
+
+                greenHousePosition = new Position(8, 22);
+                greenHouseSize = new Size(5, 6);
+
+                lakePosition = new Position(54, 9);
+                lakeSize = new Size(5, 5);
+
+                quarterPosition = new Position(78, 80);
+                quarterSize = new Size(6, 4);
+                break;
+            }
+            case 1: {
+                housePosition = new Position(66, 5);
+                houseSize = new Size(4, 4);
+
+                greenHousePosition = new Position(13, 73);
+                greenHouseSize = new Size(5, 6);
+
+                lakePosition = new Position(21, 14);
+                lakeSize = new Size(4, 5);
+
+                quarterPosition = new Position(83, 65);
+                quarterSize = new Size(4, 5);
+                break;
+            }
+            case 2: {
+                housePosition = new Position(74, 59);
+                houseSize = new Size(4, 4);
+
+                greenHousePosition = new Position(31, 15);
+                greenHouseSize = new Size(5, 6);
+
+                lakePosition = new Position(81, 9);
+                lakeSize = new Size(6, 5);
+
+                quarterPosition = new Position(22, 68);
+                quarterSize = new Size(6, 6);
+                break;
+            }
+            case 3: {
+                housePosition = new Position(21, 75);
+                houseSize = new Size(4, 4);
+
+                greenHousePosition = new Position(82, 17);
+                greenHouseSize = new Size(5, 6);
+
+                lakePosition = new Position(15, 12);
+                lakeSize = new Size(6, 5);
+
+                quarterPosition = new Position(58, 73);
+                quarterSize = new Size(6, 6);
+                break;
+            }
+            default: {
+                housePosition = null;
+                houseSize = null;
+                greenHousePosition = null;
+                greenHouseSize = null;
+                lakePosition = null;
+                lakeSize = null;
+                quarterPosition = null;
+                quarterSize = null;
+                break;
+            }
+        }
 //        TODO: build starting map of farm
     }
 
+    public Size getSize() {
+        return size;
+    }
+
+    public Position getHousePosition() {
+        return housePosition;
+    }
+
+    public Size getHouseSize() {
+        return houseSize;
+    }
+
+    public Position getGreenHousePosition() {
+        return greenHousePosition;
+    }
+
+    public Size getGreenHouseSize() {
+        return greenHouseSize;
+    }
+
+    public Position getLakePosition() {
+        return lakePosition;
+    }
+
+    public Size getLakeSize() {
+        return lakeSize;
+    }
+
+    public Position getQuarterPosition() {
+        return quarterPosition;
+    }
+
+    public Size getQuarterSize() {
+        return quarterSize;
+    }
 }
