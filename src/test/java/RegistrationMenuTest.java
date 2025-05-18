22d293bd75b2ee481d45527b39b736746bf0001a
nepticoo <mahdi.ab5310@gmail.com>
2025-05-18 07:56:02 +0330

    Unit test completed

--- src/test/java/RegistrationMenuTest.java
+++ src/test/java/RegistrationMenuTest.java
@@ -5,8 +5,6 @@ import Views.RegistrationMenu;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -54,16 +52,12 @@ public class RegistrationMenuTest {
                 "register -usskq lala\n",
                 "register -u alai -p ksks\n",
                 "register -u test -p Test1! Test1! -n test -e test@gmail.com -g male\n",
+                "login -u msskal\n"
         };
-        String[] expecteds = {
-                "invalid command!",
-                "invalid command!",
-                "invalid command!",
-                "invalid command!",
-        };
+        String expected = "invalid command!";
 
         for (int i = 0; i < inputs.length; i++) {
-            checkOutput(inputs[i], expecteds[i]);
+            checkOutput(inputs[i], expected);
         }
     }
 
@@ -86,7 +80,7 @@ public class RegistrationMenuTest {
     }
 
     @Test
-    public void testRegisterRepeatedUsername(){
+    public void testRegisterRepeatedUsername() {
         controller.Register("test", "Test1!", "Test1!", "test", "test@test.com", "male");
         String output = controller.Register("test", "Test123!", "Test123!", "nope", "tada@gamil.com", "female").message();
         String expected = "this username is already taken";
@@ -94,7 +88,7 @@ public class RegistrationMenuTest {
     }
 
     @Test
-    public void testRegisterInvalidUsername(){
+    public void testRegisterInvalidUsername() {
         controller.Register("test!_hsh", "Test1!", "Test1!", "test", "test@test.com", "male");
         String output = controller.Register("test!_hsh", "Test1!", "Test1!", "test", "test@test.com", "male").message();
         String expected = "Username format is invalid";
@@ -143,6 +137,65 @@ public class RegistrationMenuTest {
         expected = "Email format is invalid";
         assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
 
+    }
+
+    @Test
+    public void testRegisterInvalidPassword() {
+        String expected, output;
+
+        output = controller.Register("test", "Te!1", "Te!1", "test", "test@gmail.com", "male").message();
+        expected = "Password must be at least 5 characters";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
+
+        output = controller.Register("test", "TEEE!1", "TEEE!1", "test", "test@gmail.com", "male").message();
+        expected = "Password doesn't have lowercase letters'";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
+
+        output = controller.Register("test", "teee!1", "teee!1", "test", "test@gmail.com", "male").message();
+        expected = "Password doesn't have uppercase letters'";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
+
+        output = controller.Register("test", "Teee!ee", "Teee!ee", "test", "test@gmail.com", "male").message();
+        expected = "Password doesn't have digits";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
+
+        output = controller.Register("test", "Teee1ee", "Teee1ee", "test", "test@gmail.com", "male").message();
+        expected = "Password doesn't have special characters";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
+
+        output = controller.Register("test", "Test1!", "Test2!", "test", "test@gmail.com", "male").message();
+        expected = "Your Password confirmation is incorrect";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
+    }
+
+    @Test
+    public void testRegisterRandomPassword() {
+        String output = controller.Register("test", "random", "random", "test", "test@gmail.com", "male").message();
+        String expected = "This is a random password:";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
+    }
+
+    @Test
+    public void testLoginValidUserValidPassword() {
+        controller.Register("test", "Test1!", "Test1!", "test", "test@test.com", "male");
+        String output = controller.login("test", "Test1!", false).message();
+        String expected = "You logged in successfully!";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
+    }
+
+    @Test
+    public void testLoginInvalidUser() {
+        String output = controller.login("testTEST", "Test1!", false).message();
+        String expected = "Wrong Username";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
+    }
+
+    @Test
+    public void testLoginValidUserInvalidPassword() {
+        controller.Register("test", "Test1!", "Test1!", "test", "test@test.com", "male");
+        String output = controller.login("test", "Test2!", false).message();
+        String expected = "Wrong Password";
+        assertTrue(output.startsWith(expected), "Expected output: " + expected + "\nActual output: " + output);
     }
 
 
